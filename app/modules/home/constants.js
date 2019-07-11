25a39249381dce307e0692476363173c8c5650c1
rayswilling <swillingray@gmail.com>
2019-07-11 14:28:29 +0100

    api added, still not showing

---
+++ app/modules/home/constants.js
@@ -0,0 +1,11 @@
+// import  '../../.env';
+
+export const NAME = 'home';
+
+export const RETRIEVING_HEADLINES = 'home/RETRIEVING_HEADLINES';
+export const HEADLINES_AVAILABLE = 'home/HEADLINES_AVAILABLE';
+export const HEADLINES_ERROR = 'home/HEADLINES_ERROR';
+
+export const API_URL = 'https://newsapi.org/v2/top-headlines?';
+export const GOOGLE_API_KEY = ["cf4ccfc3a354423db8ffd79e7a7d0583"];
+
